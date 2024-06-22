6c57a535b63e2957bf44da004d030966c03837d8
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-06-22 14:04:32 +0700

    feat: add image form

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,35 @@
+import Header from "@/components/shared/Header";
 import React from "react";
+import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { auth } from "@clerk/nextjs/server";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId: clerkId } = auth();
+  const transformation = transformationTypes[type];
+
+  if (!clerkId) redirect("/sign-in");
+
+  const user = await getUserById(clerkId);
+
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subtitle} />
+
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
+  );
 };
 
 export default AddTransformationTypePage;
