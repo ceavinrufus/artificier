0ca26340be4f727f1205a1937159de0a2cb4920d
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-06-22 18:53:25 +0700

    style: branding

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,7 +14,8 @@ const Home = async ({ searchParams }: SearchParamProps) => {
     <>
       <section className="home">
         <h1 className="home-heading">
-          Unleash Your Creative Vision with Artificier!
+          Turn Ideas into Reality. Simply With{" "}
+          <span className="underline">Artificier</span>.
         </h1>
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
